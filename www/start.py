d1d42a88c00eb8d0d4cf5052240b2a0b1cb7caf5
Jayson-Tolleson <30577311+Jayson-Tolleson@users.noreply.github.com>
2024-04-23 18:06:02 -0700

    Update start.py

--- www/start.py
+++ www/start.py
@@ -1,7 +1,7 @@
 import subprocess
 from subprocess import Popen, PIPE, STDOUT, run
 
-start = ('sudo nohup python3 /var/www/fishmaplibre.py & sudo nohup python3 /var/www/roboweather.py & sudo nohup python3 /var/www/cgi-bin/upload.py &')
+start = ('sudo nohup python3 /var/www/fishmaplibre.py & sudo nohup python3 /var/www/roboweather.py &')
 subprocess.run(start, shell=True, stderr=subprocess.STDOUT)
 
 
